dc3492ff6d4a666859650f08633f213dcac7de21
Ritam Samanta <bruceritamwayne02@gmail.com>
2023-09-10 15:12:51 +0530

    few changes added

--- backend/src/repository/user-repository.js
+++ backend/src/repository/user-repository.js
@@ -1,54 +1,51 @@
-const { User } = require('../models/index1');
+const { User } = require("../models/user");
 
 class UserRepository {
-    async createUser({ name }){
-        try {
-            const user = await user.create({ name });
-            return user;
-            
-        } catch (error) {
-            console.log("Something went wrong in the repository layer");
-            throw {error};
-        }
+  async createUser({ name }) {
+    try {
+      const user = await user.create({ name });
+      return user;
+    } catch (error) {
+      console.log("Something went wrong in the repository layer");
+      throw { error };
     }
+  }
 
-    async deleteUser(userId){
-            try {
-                await User.destroy({
-                    where: {
-                        id: userId
-                    },
-                });
-            } catch (error) {
-                console.log("Something went wrong in the repository layer");
-                throw {error};
-            }
+  async deleteUser(userId) {
+    try {
+      await User.destroy({
+        where: {
+          id: userId,
+        },
+      });
+    } catch (error) {
+      console.log("Something went wrong in the repository layer");
+      throw { error };
     }
+  }
 
-    async updateUser(userId,data){
-        try {
-            const user = await User.update(data,{
-                where: {
-                    id : userId
-                }
-            });
-            return user;
-        } catch (error) {
-            console.log("Something went wrong in the repository layer");
-            throw {error};
-            
-        }
+  async updateUser(userId, data) {
+    try {
+      const user = await User.update(data, {
+        where: {
+          id: userId,
+        },
+      });
+      return user;
+    } catch (error) {
+      console.log("Something went wrong in the repository layer");
+      throw { error };
     }
-    async getUser(userId){
-        try {
-            const user = await User.findbyPK(userId);
-            return user;
-        } catch (error) {
-            console.log("Something went wrong in the repository layer ");
-            throw {error};
-            
-        }
+  }
+  async getUser(userId) {
+    try {
+      const user = await User.findbyPK(userId);
+      return user;
+    } catch (error) {
+      console.log("Something went wrong in the repository layer ");
+      throw { error };
     }
+  }
 }
 
 module.exports = UserRepository;
