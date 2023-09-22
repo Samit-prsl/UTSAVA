a8c55d09cd3f4261acd71c493ad88a17a4a47854
Samit <Samit.prsl@gmail.com>
2023-09-22 14:42:29 +0530

    typescriptized existing javascript frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,4 @@
+import * as React from 'react'
 import './App.css';
 import { BrowserRouter as Router,
 Routes,
@@ -10,19 +11,19 @@ import Login from './Pages/Login';
 import Gallery from './Pages/Gallery';
 import Contact from './Pages/Contact';
 import Venues from './Pages/Venues';
-import Services from './Pages/Services';
+import Services from './Pages/Services'
 function App() {
   return (
     <>
     <Router>
       <Routes>
-        <Route exact path='/' Component={Homepage} />
-        <Route path='/register' Component={Register}/>
-        <Route path='/login' Component={Login}/>
-        <Route path='/gallery' Component={Gallery}/>
-        <Route path='/contact' Component={Contact}/>
-        <Route path='/venues' Component={Venues}/>
-        <Route path='/services' Component={Services}/>
+        <Route path='/' element={<Homepage/>} />
+        <Route path='/register' element={<Register/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/gallery' element={<Gallery/>}/>
+        <Route path='/contact' element={<Contact/>}/>
+        <Route path='/venues' element={<Venues/>}/>
+        <Route path='/services' element={<Services/>}/>
       </Routes>
     </Router>
     </>
